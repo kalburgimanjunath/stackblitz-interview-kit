69e66f0bf4154e6d04fa90550193f9ce90b56ef3
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-31 11:58:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# algo today
+
+# all company wise interview questions
+
+# theory and tutorials
+
+# best examples
+
+# solve yourself
+
+# analysis and theses
---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>Tweet</li>
+        <li>Algochurn</li>
+        <li>Frontendchurn</li>
+        <li>Companies</li>
+        <li>Blog</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  );
+}
