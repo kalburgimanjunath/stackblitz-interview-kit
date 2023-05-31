90b7e414d6404f9efe7a0a770eeac4a52e49e935
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-31 13:06:41 +0530

    1 file modified

--- pages/frontend.js
+++ pages/frontend.js
@@ -1,7 +1,47 @@
+import Card from '../components/Card';
+
 export default function Frontend() {
+  const alogos = [
+    {
+      id: 1,
+      title: 'Levenshtein Distance',
+      description:
+        'Given 2 strings, find the minimum number of operations required to change one string to the other. An operation is defined as either replacement of a',
+    },
+    {
+      id: 2,
+      title: 'Minimum Number Of Coins To Make Change',
+      description:
+        'Given an array of coins or denominations and a target sum, calculate the minimum number of coins required to match the total. Note that the coins arra',
+    },
+    {
+      id: 3,
+      title: 'Number of Ways To Make Change',
+      description:
+        'Given a target amount and a set of denominations (coins), find the total number of ways the given target amount can be expressed by using the denomina',
+    },
+  ];
   return (
-    <div className="container bg-gray-200">
-      <div className="font-bold">Frontend Problems</div>
+    <div className="container bg-gray-200 p-5">
+      <div className="font-bold p-2">Frontend Problems</div>
+      <div className="bg-white-100 flex grid grid-cols-3 grid-cols-xs-2 divide-y divide-gray-200">
+        {alogos &&
+          alogos.map((item) => {
+            return <Card item={item} />;
+          })}
+      </div>
+      <div className="bg-white-100 flex grid grid-cols-3 grid-cols-xs-2 divide-y divide-gray-200">
+        {alogos &&
+          alogos.map((item) => {
+            return <Card item={item} />;
+          })}
+      </div>
+      <div className="bg-white-100 flex grid grid-cols-3 grid-cols-xs-2 divide-y divide-gray-200">
+        {alogos &&
+          alogos.map((item) => {
+            return <Card item={item} />;
+          })}
+      </div>
     </div>
   );
 }
