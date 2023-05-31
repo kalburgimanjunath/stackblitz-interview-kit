e369b3e458162818ebcd3ab00fbb55eeb080f88e
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-31 17:05:49 +0530

    3 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Header from '../components/Header';
 
 export default function Home() {
-  return <div className="container mx-auto bg-gray-100">Home</div>;
+  return <div className="container mx-auto bg-gray-100"></div>;
 }
