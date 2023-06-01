90541290cd864bbf1e3fa2052c532dd11815b950
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-06-01 18:40:13 +0530

    2 files modified

--- pages/contribute.js
+++ pages/contribute.js
@@ -1,94 +1,230 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function Contribute() {
+  const saveData = (data) => {
+    const url = `https://api.airtable.com/v0/appXy3Z9EQGEQveKp/Questions`;
+    const requestOptions = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer keyeNXyxxuuYJY19w',
+      },
+      body: JSON.stringify(data),
+      typecast: true,
+    };
+    console.log(requestOptions);
+    fetch(url, requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="max-w-xl mx-auto py-12 md:max-w-4xl">
       <h4 className="text-2xl font-bold">Contribute problem to community </h4>
-      <div className="mt-8 grid grid-cols-2 md:grid-cols-2 gap-6 items-start">
-        <label class="block">
-          <span class="text-gray-700">Title</span>
-          <input
-            type="text"
-            class="form-input mt-1 block  w-full"
-            placeholder="john@example.com"
-          />
-        </label>
-        <label class="block">
-          <span class="text-gray-700"> Difficulty</span>
-          <select
-            type="select"
-            class="form-select mt-1 block  w-full"
-            placeholder="john@example.com"
-          >
-            <option>Select Difficulty</option>
-          </select>
-        </label>
-        <label class="block">
-          <span class="text-gray-700"> Problem Statement</span>
-          <textarea
-            type="text"
-            class="form-textarea mt-1 block  w-full"
-            placeholder="john@example.com"
-          />
-        </label>
-        <label class="block">
-          <span class="text-gray-700"> Solution</span>
-          <textarea
-            type="text"
-            class="form-textarea mt-1 block  w-full"
-            placeholder="john@example.com"
-          />
-        </label>
-        <label class="block">
-          <span class="text-gray-700"> Date</span>
-          <input
-            type="text"
-            class="form-input mt-1 block  w-full"
-            placeholder="john@example.com"
-          />
-        </label>
-        <label class="block">
-          <span class="text-gray-700"> Sample Code</span>
-          <textarea
-            type="text"
-            class="form-textarea mt-1 block  w-full"
-            placeholder="john@example.com"
-          />
-        </label>
-        <label class="block">
-          <span class="text-gray-700"> Tags</span>
-          <input
-            type="text"
-            class="form-input mt-1 block  w-full"
-            placeholder="john@example.com"
-          />
-        </label>
-        <label class="block">
-          <span class="text-gray-700"> Language</span>
-          <input
-            type="text"
-            class="form-input mt-1 block  w-full"
-            placeholder="john@example.com"
-          />
-        </label>
-        <label class="block">
-          <span class="text-gray-700"> Companies</span>
-          <input
-            type="text"
-            class="form-input mt-1 block  w-full"
-            placeholder="john@example.com"
-          />
-        </label>
-        <label class="block">
-          <span class="text-gray-700"> Slug</span>
-          <input
-            type="text"
-            class="form-input mt-1 block  w-full"
-            placeholder="john@example.com"
-          />
-        </label>
+      <div className="mt-8 grid grid-cols-1 md:grid-cols-1 gap-6 items-start">
+        <Formik
+          initialValues={{ title: '' }}
+          // validate={(values) => {
+          //   const errors = {};
+          //   if (!values.title) {
+          //     errors.title = 'Required';
+          //   }
+          //   if (!values.description) {
+          //     errors.description = 'Required';
+          //   }
+          //   if (!values.content) {
+          //     errors.content = 'Required';
+          //   }
+          //   if (!values.tags) {
+          //     errors.tags = 'Required';
+          //   }
+          //   if (!values.category) {
+          //     errors.category = 'Required';
+          //   }
+          //   return errors;
+          // }}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            setTimeout(() => {
+              toast('Successfully saved');
+              saveData({
+                fields: values,
+              });
+              resetForm({
+                values: {
+                  title: '',
+                  difficulty:'',
+                  problemStatement:'',
+                  solution:'',
+                  date:'',
+                  samplecode:'',
+                  tags:'',
+                  language:'',
+                  companies:'',
+                  slug:''
+
+                },
+              });
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            resetForm,
+          }) => (
+            <>
+              <Form>
+                <ToastContainer />
+                <div className="grid grid-cols-2 gap-6">
+                  <label className="block">
+                    <span className="text-gray-700">Title</span>
+                    <input
+                      type="text"
+                      name="title"
+                      className="form-input mt-1 block  w-full"
+                      value={values.title}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder=""
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-gray-700"> Difficulty</span>
+                    <select
+                      type="select"
+                      name="difficulty"
+                      className="form-select mt-1 block  w-full"
+                      value={values.difficulty}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="john@example.com"
+                    >
+                      <option value="">Select Difficulty</option>
+                      <option value="Easy">Easy</option>
+                      <option value="Medium">Medium</option>
+                      <option value="Hard">Hard</option>
+                    </select>
+                  </label>
+                  <label className="block">
+                    <span className="text-gray-700"> Problem Statement</span>
+                    <textarea
+                      type="text"
+                      name="problemStatement"
+                      className="form-textarea mt-1 block  w-full"
+                      value={values.problemStatement}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="john@example.com"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-gray-700"> Solution</span>
+                    <textarea
+                      type="text"
+                      className="form-textarea mt-1 block  w-full"
+                      name="solution"
+                      value={values.solution}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="john@example.com"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-gray-700"> Date</span>
+                    <input
+                      type="text"
+                      className="form-input mt-1 block  w-full"
+                      name="date"
+                      value={values.date}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="john@example.com"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-gray-700"> Sample Code</span>
+                    <textarea
+                      type="text"
+                      className="form-textarea mt-1 block  w-full"
+                      name="samplecode"
+                      value={values.samplecode}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="john@example.com"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-gray-700"> Tags</span>
+                    <input
+                      type="text"
+                      className="form-input mt-1 block  w-full"
+                      name="tags"
+                      value={values.tags}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="john@example.com"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-gray-700"> Language</span>
+                    <input
+                      type="text"
+                      className="form-input mt-1 block  w-full"
+                      placeholder="john@example.com"
+                      name="language"
+                      value={values.language}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-gray-700"> Companies</span>
+                    <input
+                      type="text"
+                      className="form-input mt-1 block  w-full"
+                      placeholder="john@example.com"
+                      name="companies"
+                      value={values.companies}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="text-gray-700"> Slug</span>
+                    <input
+                      type="text"
+                      className="form-input mt-1 block  w-full"
+                      placeholder="john@example.com"
+                      name="slug"
+                      value={values.slug}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  </label>
+                </div>
+                <button
+                  className="mt-2 p-2 bg-blue-700 text-gray-100 hover:bg-blue-500 text-gray-200 w-full "
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Save
+                </button>
+              </Form>
+            </>
+          )}
+        </Formik>
       </div>
-      <button class="mt-2 p-2 bg-blue-700 text-gray-100 hover:bg-blue-500 text-gray-200 w-full ">
+      {/* <button className="mt-2 p-2 bg-blue-700 text-gray-100 hover:bg-blue-500 text-gray-200 w-full ">
         Save
-      </button>
+      </button> */}
     </div>
   );
 }
