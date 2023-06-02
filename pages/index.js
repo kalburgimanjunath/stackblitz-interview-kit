0008c1f8bed6bb9fc6ca1adece6ca39cc15cb53b
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-06-02 16:13:42 +0530

    1 binaries uploaded

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import Header from '../components/Header';
+import BackgroundSlideshow from 'react-background-slideshow';
 
+import image1 from 'https://picsum.photos/200/300';
+import image2 from 'https://picsum.photos/200/300';
+import image3 from 'https://picsum.photos/200/300';
 export default function Home() {
-  return <div className="container mx-auto bg-gray-100"></div>;
+  return (
+    <div className="container mx-auto bg-gray-100">
+      <BackgroundSlideshow images={[image1, image2, image3]} />
+    </div>
+  );
 }
