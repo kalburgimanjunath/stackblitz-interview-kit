59fbd084581c72683d73e380a7e232ce389f1423
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-06-02 16:22:08 +0530

    1 file modified, 1 added, 1 deleted

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Header from '../components/Header';
 import BackgroundSlideshow from 'react-background-slideshow';
 
-import image1 from 'https://picsum.photos/200/300';
-import image2 from 'https://picsum.photos/200/300';
-import image3 from 'https://picsum.photos/200/300';
+import image1 from '../assets/001.jpg';
+import image2 from '../assets/001.jpg';
+import image3 from '../assets/001.jpg';
 export default function Home() {
   return (
     <div className="container mx-auto bg-gray-100">
-      <BackgroundSlideshow images={[image1, image2, image3]} />
+      {/* <BackgroundSlideshow images={[image1, image2, image3]} /> */}
     </div>
   );
 }
