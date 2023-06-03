19a20d28848032989f97ee03fc1499b048648c6c
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-06-03 13:18:16 +0530

    3 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,44 @@
 import Header from '../components/Header';
 import BackgroundSlideshow from 'react-background-slideshow';
-
-import image1 from '../assets/001.jpg';
-import image2 from '../assets/001.jpg';
-import image3 from '../assets/001.jpg';
+import {
+  MainComponent,
+  Button,
+  Page,
+  Navbar,
+  Posts,
+  Hero,
+} from 'mynewslds-library';
+import 'mynewslds-library/dist/output.css';
+// import image1 from '../assets/001.jpg';
+// import image2 from '../assets/001.jpg';
+// import image3 from '../assets/001.jpg';
 export default function Home() {
+  const posts = [
+    {
+      userId: 1,
+      id: 1,
+      title:
+        'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      image: 'https://picsum.photos/seed/picsum/200/300',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'qui est esse',
+      image: 'https://picsum.photos/seed/picsum/200/300',
+      body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+    },
+  ];
   return (
     <div className="container mx-auto bg-gray-100">
       {/* <BackgroundSlideshow images={[image1, image2, image3]} /> */}
+      <MainComponent
+        text="Create React Library Example 😄"
+        backgroundColor="orange"
+      />
+      <Hero />
+      <Posts posts={posts} />
     </div>
   );
 }
