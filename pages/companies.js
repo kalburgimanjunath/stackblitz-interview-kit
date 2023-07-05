dcd836da239ad287fff42e4db68cdc8dd810a056
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-05 10:52:56 +0530

    1 file modified

--- pages/companies.js
+++ pages/companies.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 export default function Companies() {
   const companies = [
     'facebook',
@@ -14,6 +15,34 @@ export default function Companies() {
     'Quess',
     'Bcforward',
   ];
+  const [remoteCompany, setRemoteCompanies] = useState(null);
+  const OPTIONS = {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const getCompanies = async () => {
+    try {
+      const response = await fetch(
+        'https://www.remote-work.app/_next/data/5UEnvi3GI_dL8GfDh-H51/remote-companies.json',
+        {
+          mode: 'no-cors',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      ).then((response) => {
+        console.log(response);
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  useEffect(() => {
+    getCompanies();
+  }, []);
   const Companies = ({ item }) => {
     return (
       <Link href="../companybase">
